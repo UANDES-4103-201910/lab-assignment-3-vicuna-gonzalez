5362144132eb952dd038f3ec0ce6c28c1189ceb3
gusvicuna <gpvicuna@miuandes.cl>
2019-03-27 14:17:34 -0300

    user methods done

--- ticketsales/app/models/user.rb
+++ ticketsales/app/models/user.rb
@@ -20,8 +20,23 @@ class User < ActiveRecord::Base
   end
 
   def most_expensive_ticket_bought_between(date1,date2)
+    orders=Order.where(user_id: id)
+    orders2=orders.where(created_at<date2).where(created_at>date1)
+    tickets=[]
+    orders2.each do |order|
+      tickets.append(Ticket.find(order.ticket_id))
+    end
+    max= 0
+    tickets.each do |ticket|
+      if max < ticket.price
+        max = ticket.price
+      end
+    end
+    max
   end
 
   def last_event()
+    orders=Order.where(user_id: id)
+    Event.find((Ticket.find(orders.last.ticket_id)).event_id)
   end
 end
