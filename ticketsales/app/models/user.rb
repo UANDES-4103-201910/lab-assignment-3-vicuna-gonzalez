42b037522f6c445d07168f5d441ad07029494d19
gusvicuna <gpvicuna@miuandes.cl>
2019-03-27 11:00:59 -0300

    Starting methods

--- ticketsales/app/models/user.rb
+++ ticketsales/app/models/user.rb
@@ -2,4 +2,11 @@ class User < ActiveRecord::Base
   has_many :emails, dependent: :destroy
   has_many :phones, dependent: :destroy
   has_many :orders, dependent: :destroy
+
+  def most_expensive_ticket_bought()
+  end
+  def most_expensive_ticket_bought_between(date1,date2)
+  end
+  def last_event()
+  end
 end
