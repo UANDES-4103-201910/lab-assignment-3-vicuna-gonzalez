bd8d97f357fb36b8a00f68513c813fcf3a341d34
barracius <rjgonzalez@miuandes.cl>
2019-03-27 12:06:21 -0300

    Part 4 Done

--- ticketsales/db/schema.rb
+++ ticketsales/db/schema.rb
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 20190327125531) do
 
   create_table "eventvenues", force: :cascade do |t|
     t.string   "name"
-    t.string   "adress"
+    t.string   "address"
     t.integer  "capacity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -64,7 +64,7 @@ ActiveRecord::Schema.define(version: 20190327125531) do
     t.string   "name"
     t.string   "lastname"
     t.string   "password"
-    t.string   "adress"
+    t.string   "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
