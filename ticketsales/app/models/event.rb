1f44e675823bf4c102e006b945657c181103650a
gusvicuna <gpvicuna@miuandes.cl>
2019-03-28 11:20:17 -0300

    most ticlets sold done

--- ticketsales/app/models/event.rb
+++ ticketsales/app/models/event.rb
@@ -3,7 +3,24 @@ class Event < ActiveRecord::Base
   belongs_to :eventvenue
 
   def most_tickets_sold
-
+    max=0
+    eventmax=0
+    Event.all.each do |event|
+      tickets=Ticket.where(event_id: event.id)
+      orders=[]
+      tickets.each do |ticket|
+        orders=orders+(Order.where(ticket_id: ticket.id))
+      end
+      sold=0
+      orders.each do |ord|
+        sold=sold+ord.amount
+      end
+      if sold>=max
+        max=sold
+        eventmax=event
+      end
+    end
+    eventmax
   end
 
   def highest_revenue
